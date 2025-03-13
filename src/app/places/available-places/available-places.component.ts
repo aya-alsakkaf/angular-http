1b8f62bca043a662217c183a629d85a83bf8a9a2
Aya Alsakkaf <aya.alsakkaf@ccse.ku.edu.kw>
2025-03-13 22:13:13 +0300

    Error Handling

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -4,7 +4,7 @@ import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs';
+import { catchError, map, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -12,38 +12,41 @@ import { map } from 'rxjs';
   templateUrl: './available-places.component.html',
   styleUrl: './available-places.component.css',
   imports: [PlacesComponent, PlacesContainerComponent],
-
 })
 export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
-  private httpClient = inject(HttpClient)
-  isFetching = signal(false)
-  private destoryRef = inject(DestroyRef)
+  private httpClient = inject(HttpClient);
+  isFetching = signal(false);
+  error = signal('');
+  private destoryRef = inject(DestroyRef);
 
   ngOnInit(): void {
-
-    this.isFetching.set(true)
-    const subscription = this.httpClient.get<{ places: Place[] }>("http://localhost:3000/places", {
-      observe: 'response'
-    }).pipe().subscribe(
-      {
+    this.isFetching.set(true);
+    const subscription = this.httpClient
+      .get<{ places: Place[] }>('http://localhost:3000/places', {
+        observe: 'response',
+      })
+      .pipe(
+        catchError((error) => {
+          return throwError(() => new Error('Something Went Wrong'))
+        }
+        )
+      )
+      .subscribe({
         next: (response) => {
           console.log(response.body?.places);
-          this.places.set(response.body?.places)
-
+          this.places.set(response.body?.places);
         },
         complete: () => {
-          this.isFetching.update((prevValue) => !prevValue)
-        }
-      }
-    ) //you need to subscribe in order to trigger the request
+          this.isFetching.update((prevValue) => !prevValue);
+        },
+        error: (error: Error) => {
+          this.error.set(error.message);
+        },
+      }); //you need to subscribe in order to trigger the request
 
     this.destoryRef.onDestroy(() => {
-      subscription.unsubscribe()
-    })
+      subscription.unsubscribe();
+    });
   }
-
-
-
-
 }
