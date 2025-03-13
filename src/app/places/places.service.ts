7bb0aba22c68a31b8c4b5fbaaedc81fc1f5a1a7f
Aya Alsakkaf <aya.alsakkaf@ccse.ku.edu.kw>
2025-03-13 23:35:01 +0300

    app wide error

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -3,50 +3,59 @@ import { inject, Injectable, signal } from '@angular/core';
 import { Place } from './place.model';
 import { HttpClient } from '@angular/common/http';
 import { catchError, map, tap, throwError } from 'rxjs';
+import { ErrorService } from '../shared/error.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PlacesService {
   private userPlaces = signal<Place[]>([]);
-  private httpClient = inject(HttpClient)
+  private httpClient = inject(HttpClient);
   loadedUserPlaces = this.userPlaces.asReadonly();
-
-
+  private errorService = inject(ErrorService)
   private fetchPlaces(url: string, errorMessage: string) {
-    return this.httpClient
-      .get<{ places: Place[] }>(url)
-      .pipe(
-        map(res => res.places),
-        catchError((error) => {
-          return throwError(() => new Error(errorMessage))
-        }
-        )
-      )
+    return this.httpClient.get<{ places: Place[] }>(url).pipe(
+      map((res) => res.places),
+      catchError((error) => {
+        return throwError(() => new Error(errorMessage));
+      })
+    );
   }
   loadAvailablePlaces() {
-    return this.fetchPlaces("http://localhost:3000/places", "Something Went Wrong Fetchig The Available Places")
+    return this.fetchPlaces(
+      'http://localhost:3000/places',
+      'Something Went Wrong Fetchig The Available Places'
+    );
   }
 
   loadUserPlaces() {
-    return this.fetchPlaces("http://localhost:3000/user-places", "Something Went Wrong Fetching Your Favorite Places").pipe(tap({
-      next: (response) => this.userPlaces.set(response)
-    }))
+    return this.fetchPlaces(
+      'http://localhost:3000/user-places',
+      'Something Went Wrong Fetching Your Favorite Places'
+    ).pipe(
+      tap({
+        next: (response) => this.userPlaces.set(response),
+      })
+    );
   }
 
   addPlaceToUserPlaces(place: Place) {
-    const prevPlaces = this.userPlaces()
+    const prevPlaces = this.userPlaces();
 
     if (!prevPlaces.some((p) => p.id === place.id)) {
-
       this.userPlaces.set([...prevPlaces, place]);
     }
-    return this.httpClient.put("http://localhost:3000/user-places", {
-      placeId: place.id
-    }).pipe(catchError(error => {
-      this.userPlaces.set(prevPlaces); //rolling back to previous version
-      return throwError(() => new Error("Failed To Store Selected Place"))
-    }))
+    return this.httpClient
+      .put('http://localhost:3000/user-places', {
+        placeId: place.id,
+      })
+      .pipe(
+        catchError((error) => {
+          this.userPlaces.set(prevPlaces); //rolling back to previous version
+          this.errorService.showError('Failed To Store Selected Place')
+          return throwError(() => new Error('Failed To Store Selected Place'));
+        })
+      );
   }
 
   removeUserPlace(place: Place) { }
