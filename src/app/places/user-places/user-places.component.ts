c10d1ca7589b9bb57dbe474b525661e908e17e10
Aya Alsakkaf <aya.alsakkaf@ccse.ku.edu.kw>
2025-03-13 22:40:02 +0300

    fetching user places

--- src/app/places/user-places/user-places.component.ts
+++ src/app/places/user-places/user-places.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { PlacesComponent } from '../places.component';
+import { HttpClient } from '@angular/common/http';
+import { Place } from '../place.model';
 
 @Component({
   selector: 'app-user-places',
@@ -10,5 +12,40 @@ import { PlacesComponent } from '../places.component';
   styleUrl: './user-places.component.css',
   imports: [PlacesContainerComponent, PlacesComponent],
 })
-export class UserPlacesComponent {
+export class UserPlacesComponent implements OnInit {
+
+  private httpClient = inject(HttpClient)
+  private destroyRef = inject(DestroyRef)
+  places = signal<Place[] | undefined>(undefined);
+  isFetching = signal(false);
+  error = signal('')
+
+
+  ngOnInit(): void {
+    this.isFetching.set(true);
+    const subscription = this.httpClient.get<{ places: Place[] }>("http://localhost:3000/user-places", {
+      observe: 'response'
+    }).subscribe(
+      {
+        next: (response) => {
+          console.log("userplaces", response)
+          this.places.set(response.body?.places)
+        },
+        complete: () => {
+          this.isFetching.update((prev) => !prev)
+        },
+        error: () => {
+          this.error.set("Something Went Wrong")
+        }
+      }
+    )
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+
+  }
+
+
+
 }
